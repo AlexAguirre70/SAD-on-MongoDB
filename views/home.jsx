6bfd04fdcad068bcd4f6597686d0de8f2d8c9391
Adrian Mitre <mitrehealthcare@gmail.com>
2022-10-20 17:01:22 -0700

    Views: Home page Added

---
+++ views/home.jsx
@@ -0,0 +1,26 @@
+const React = require('react')  //Includes React in order to render jsx files in views
+
+const Def = require('./default')  //includes the default jsx file to provide the default jsx framework
+
+// the function to pass the hmt js setup for the home page
+function home () {
+    return (
+      <Def>
+          <main>
+              <h1>Software App For The Developer</h1>
+              <div>     
+                <img src="/images/hall.jpg" alt="Hall" />
+                <img src="/images/river.jpg" alt="River" />
+                <img src="/images/spacer1.jpg" alt="Spece1" />
+              </div>
+              <p>
+                <a href="/topics">
+                    <button className="btn-primary" id='placeBtn'> Topics Page</button>
+                </a>
+              </p>
+          </main>
+      </Def>
+    )
+  }
+
+module.exports = home   // This will export this module to be able to be used with other views  
