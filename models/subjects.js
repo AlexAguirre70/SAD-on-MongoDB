6c9181ed2ea3cad81d4f77ee1f342fd9ca7d4382
Alex Aguirre <102642947+AlexAguirre70@users.noreply.github.com>
2022-10-22 23:42:42 -0700

    Update subjects.js
    Signed-off-by: Alex Aguirre <102642947+AlexAguirre70@users.noreply.github.com>

--- models/subjects.js
+++ models/subjects.js
@@ -5,11 +5,7 @@ const subjectsSchema = new Schema({
     _id: mongoose.ObjectId,
     subject_id: Number,
     subject_name: {type:String, required: true},
-    subject_img_url:{type:String, required: true},
-    comments:[{type:Schema.Types.ObjectId,ref:'Comments'}],
-    topics:[{type:Schema.Types.ObjectId,ref:'Topics'}],
-    resources:[{type:Schema.Types.ObjectId,ref:'Resources'}]
-
+    subject_img_url:{type:String, required: true}
 })
 const Subjects=mongoose.model('Subjects', subjectsSchema)
 module.exports = Subjects
