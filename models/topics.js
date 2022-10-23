95f936f5ed41fe6905d9285efbd20505e6567811
Alex Aguirre <102642947+AlexAguirre70@users.noreply.github.com>
2022-10-22 23:47:33 -0700

    Update topics.js
    Signed-off-by: Alex Aguirre <102642947+AlexAguirre70@users.noreply.github.com>

--- models/topics.js
+++ models/topics.js
@@ -4,7 +4,8 @@ const {Schema} = mongoose
 const topicsSchema = new Schema({
     topic_id: Number,
     topic_subject_id:Number,
-    topic_name: String
+    topic_name: String,
+    topic_subjects:{type:Schema.Types.ObjectID, ref:Subjects}
 })
 
 const Topics=mongoose.model('Topics', topicsSchema)
