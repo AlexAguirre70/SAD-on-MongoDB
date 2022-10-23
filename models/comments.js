7cb00c42f5e9fd268e3158358c6d994e783ec9d7
Alex Aguirre <102642947+AlexAguirre70@users.noreply.github.com>
2022-10-22 22:48:54 -0700

    Update comments.js
    Signed-off-by: Alex Aguirre <102642947+AlexAguirre70@users.noreply.github.com>

--- models/comments.js
+++ models/comments.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose')
+const {Schema}=mongoose
 
-let commentSchema = new mongoose.Schema({
+const commentSchema = new Schema({
     comment_id:Number,
     comment_text: {type:String, required:true},
     comment_topic_id: Number,
     comment_user: {type:String, default:'Anonymous'},
     comment_date: {type:Date, default: Date.now()},
 })
-
-module.exports = mongoose.model('Comments', commentSchema)
+const Comments=mongoose.model('Comments', subjectsSchema)
+module.exports = Comments
