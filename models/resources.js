0f688735876f62677b8c6c007f2197e333727c9a
Alex Aguirre <102642947+AlexAguirre70@users.noreply.github.com>
2022-10-22 22:47:40 -0700

    Update resources.js
    Signed-off-by: Alex Aguirre <102642947+AlexAguirre70@users.noreply.github.com>

--- models/resources.js
+++ models/resources.js
@@ -1,11 +1,11 @@
 const mongoose = require('mongoose')
-
-let resourcesSchema = new mongoose.Schema({
+const {Schema}=mongoose
+const resourcesSchema = new Schema({
     resources_id: Number,
     resources_name: {type:String, required: true},
     resources_link: {type:String, required: true},
     resources_type: {type:String, default: 'Online Guide'},
     resources_topic_id: Number
 })
-
-module.exports = mongoose.model('Resources', resourcesSchema)
+const Resources=mongoose.model('Resources', resourcesSchema)
+module.exports = Resources
